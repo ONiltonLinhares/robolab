9e860fe118c69a0ccbd382278539b7512970e58d
nilton linhares <nilton.linhares.n@gmail.com>
2024-01-20 22:00:35 -0300

    UPDATEE 1.0

--- scripts.js
+++ scripts.js
@@ -29,6 +29,10 @@ function ajustarFlexbox(estaAtivado) {
 // Atualiza a função toggleSize para incluir o ajuste do Flexbox
 function toggleSize() {
     var crc = document.getElementById('crc');
+
+    if(crc.innerHTML === ""){
+        return 0
+    }
     
     // Checa o status atual baseado no data attribute
     var estaAtivado = crc.getAttribute('data-toggled') === 'true';
@@ -59,17 +63,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var original_cre = contentContainer.innerHTML
 
 
-    toggleButton.addEventListener("click", function() {
-
+    function toggleContent() {
+        if(crc.innerHTML === ""){
+            return 0
+        }
         contentContainer.classList.toggle("alterado", !estilosAlterados);
-
+    
         // Executa a ação relacionada ao innerHTML
         if (contentContainer.innerHTML.trim() === "") {
-
             if (isMobile()) {
-                setTimeout(function() {
-                    toggleButton.innerHTML = original_crc;
-                }, 300);
+                toggleButton.innerHTML = original_crc;
+                toggleButton.style.fontFamily = "FamiljenGrotesk-bold";
                 setTimeout(function() {
                     contentContainer.innerHTML = original_cre;
                 }, 620);
@@ -77,36 +81,88 @@ document.addEventListener("DOMContentLoaded", function() {
                 setTimeout(function() {
                     contentContainer.innerHTML = original_cre;
                     toggleButton.innerHTML = original_crc;
+                    toggleButton.style.fontFamily = "FamiljenGrotesk-bold";
                 }, 300);
             }
         } else {
             contentContainer.innerHTML = "";
             setTimeout(function() {
-                toggleButton.innerHTML = '<ul><li><a href="https://www.instagram.com/_robolab/" target="_blank">intagram: @_robolab</a></li><li><a href="mailto:robolab.ltda@gmail.com" target="_blank">email: robolab.ltda@gmail.com</a></li><li><a href="https://wa.me/557991556000" target="_blank">Contato: (55) 79 991556000</a></li><li><a href="https://www.google.com/maps/place/R.+Comendador+Manoel+Gon%C3%A7alves,+6+-+Grageru,+Aracaju+-+SE,+49027-050/@-10.944681,-37.0668118,20.5z/data=!4m6!3m5!1s0x71ab3c363c545cb:0xbb1e5de4816f2612!8m2!3d-10.9446513!4d-37.0667206!16s%2Fg%2F11hbgjyk0s?entry=ttu" target="_blank">localização: R. Comendador Manoel Gonçalves, 06 - Grageru, Aracaju - SE, 49027-050</a></li></ul><div class="contato"><p>Entrar em contato</p></div>';
-            }, 300);
+                toggleButton.innerHTML = '<ul><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://www.instagram.com/_robolab/" target="_blank">Instagram: @_robolab</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="mailto:robolab.ltda@gmail.com" target="_blank">Email: robolab.ltda@gmail.com</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://wa.me/557991556000" target="_blank">Contato: +55 (79) 991556000</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://www.google.com/maps/place/R.+Comendador+Manoel+Gon%C3%A7alves,+6+-+Grageru,+Aracaju+-+SE,+49027-050/@-10.944681,-37.0668118,20.5z/data=!4m6!3m5!1s0x71ab3c363c545cb:0xbb1e5de4816f2612!8m2!3d-10.9446513!4d-37.0667206!16s%2Fg%2F11hbgjyk0s?entry=ttu" target="_blank">localização: R. Comendador Manoel Gonçalves, 06 - Grageru, Aracaju - SE, 49027-050</a></li></ul><div class="contato"><p>Entrar em contato</p></div>';
+                toggleButton.style.fontFamily = 'AeionMono', 'Regular';
+            }, 500);
         }
-
+    
         estilosAlterados = !estilosAlterados;
-    });
+    }
+    
+    toggleButton.addEventListener("click", toggleContent);
+    listButton.addEventListener("click", toggleContent);
 
-    listButton.addEventListener("click", function() {
+    toggleButton.addEventListener("click", ajustecve());
+    listButton.addEventListener("click", ajustecve());
+});
 
-        contentContainer.classList.toggle("alterado", !estilosAlterados);
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var crc = document.getElementById("crc");
+    var cre = document.getElementById("cre");
+    var cve = document.getElementById("cve");
+
+    var estilosAlterados = false;
+
+    var original_crc = crc.innerHTML;
+    var original_cre = cre.innerHTML;
+    var original_cve = cve.innerHTML;
+
+    var original_crc_altura = cve.style.height;
+    var original_cre_altura = crc.style.height;
+    var original_cve_altura = cre.style.height;
+
+    function ajustecve() {
+        if (crc.innerHTML.trim() == '<ul><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://www.instagram.com/_robolab/" target="_blank">Instagram: @_robolab</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="mailto:robolab.ltda@gmail.com" target="_blank">Email: robolab.ltda@gmail.com</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://wa.me/557991556000" target="_blank">Contato: +55 (79) 991556000</a></li><li><img src="/imgs/svg/Elemento - 6.svg"><a href="https://www.google.com/maps/place/R.+Comendador+Manoel+Gon%C3%A7alves,+6+-+Grageru,+Aracaju+-+SE,+49027-050/@-10.944681,-37.0668118,20.5z/data=!4m6!3m5!1s0x71ab3c363c545cb:0xbb1e5de4816f2612!8m2!3d-10.9446513!4d-37.0667206!16s%2Fg%2F11hbgjyk0s?entry=ttu" target="_blank">localização: R. Comendador Manoel Gonçalves, 06 - Grageru, Aracaju - SE, 49027-050</a></li></ul><div class="contato"><p>Entrar em contato</p></div>' ){
+            return 0;
+        }
+        crc.classList.toggle("alterado", !estilosAlterados);
+
+        // Adiciona uma transição suave para as mudanças de altura
+        cve.style.transition = "height 1s ease";
+        crc.style.transition = "height 1s ease";
+        cre.style.transition = "height 1s ease";
 
         // Executa a ação relacionada ao innerHTML
-        if (contentContainer.innerHTML.trim() === "") {
+        if (crc.innerHTML.trim() === "") {
+            cve.style.height = original_cve_altura;
             setTimeout(function() {
-                contentContainer.innerHTML = original_cre;
-                toggleButton.innerHTML = original_crc;
-            }, 300);
-        } else {
-            
-            contentContainer.innerHTML = "";
-            setTimeout(function() {
-                toggleButton.innerHTML = '<ul><li><a href="https://www.instagram.com/_robolab/" target="_blank">intagram: @_robolab</a></li><li><a href="mailto:robolab.ltda@gmail.com" target="_blank">email: robolab.ltda@gmail.com</a></li><li><a href="https://wa.me/557991556000" target="_blank">Contato: (55) 79 991556000</a></li><li><a href="https://www.google.com/maps/place/R.+Comendador+Manoel+Gon%C3%A7alves,+6+-+Grageru,+Aracaju+-+SE,+49027-050/@-10.944681,-37.0668118,20.5z/data=!4m6!3m5!1s0x71ab3c363c545cb:0xbb1e5de4816f2612!8m2!3d-10.9446513!4d-37.0667206!16s%2Fg%2F11hbgjyk0s?entry=ttu" target="_blank">localização: R. Comendador Manoel Gonçalves, 06 - Grageru, Aracaju - SE, 49027-050</a></li></ul><div class="contato"><p>Entrar em contato</p></div>';
-            }, 300);
-        }
+                crc.style.height = original_crc_altura;
+                cre.style.height = original_cre_altura;
+            }, 30);
 
+            cve.innerHTML = original_cve
+
+            setTimeout(function() {
+                crc.innerHTML = original_crc;
+                cre.innerHTML = original_cre;
+            }, 40);
+        } else {
+            crc.innerHTML = "";
+            cre.innerHTML = "";
+            cve.innerHTML = '<div class="texto_cve"><p>Treinamos alunos de <u>10 a 14 anos</u> para construir e programar robos destinados a competições</p><p>Nossa metodologia alia o conhecimento tecnico com a <u>mentalidade esportiva</u>.</p></div><div class="imagens"><img src="https://raw.githubusercontent.com/ONiltonLinhares/robolab/main/imgs/svg/clube%20de%20robotica.svg" style="height: 23px;"><img src="/imgs/svg/mais.svg" style="height: 23px;"></div>';
+
+            if (isMobile()) {
+                cve.style.height = "64vh";
+            } else {
+                cve.style.height = "30vw";
+            }
+            crc.style.height = "5vw";
+            cre.style.height = "5vw";
+        }
         estilosAlterados = !estilosAlterados;
-    });
+    }
+
+    cve.addEventListener("click", ajustecve);
 });
